38daa6f5815d7985c6c3c736eb5c56e5d6d48d78
nahuelmontesino <montesinonahuel@gmail.com>
2021-09-21 18:17:17 -0300

    basic db operacion with entities

---
+++ Cuestionarios/Cuestionarios/DAL/EntityFramework/Repository.cs
@@ -0,0 +1,92 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Cuestionarios.DAL.EntityFramework
+{
+    abstract class Repository<TEntity, TDbContext>: IRepository<TEntity>
+        where TEntity: class
+        where TDbContext: DbContext
+    {
+        protected readonly TDbContext iDbContext;
+
+        public Repository(TDbContext pContext)
+        {
+            try
+            {
+                iDbContext = pContext;
+            }
+            catch (Exception ex)
+            {
+                throw new NpgsqlException(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Adds an entity
+        /// </summary>
+        public void Add(TEntity pEntity)
+        {
+            try
+            {
+                iDbContext.Set<TEntity>().Add(pEntity);
+            }
+            catch (Exception ex)
+            {
+                throw new NpgsqlException(ex.ToString());
+            }
+
+        }
+
+        /// <summary>
+        /// Gets the entity by its id
+        /// </summary>
+        public TEntity GetByID(int pId)
+        {
+            try
+            {
+                return iDbContext.Set<TEntity>().Find(pId);
+            }
+            catch (Exception ex)
+            {
+                throw new NpgsqlException(ex.ToString());
+            }
+
+        }
+
+        /// <summary>
+        /// Gets all entities
+        /// </summary>
+        public IEnumerable<TEntity> GetAll()
+        {
+            try
+            {
+                return iDbContext.Set<TEntity>().ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new NpgsqlException(ex.ToString());
+            }
+
+        }
+
+
+        /// <summary>
+        /// Remove an entity
+        /// </summary>
+        public void Remove(TEntity pEntity)
+        {
+            try
+            {
+                iDbContext.Set<TEntity>().Remove(pEntity);
+            }
+            catch (Exception ex)
+            {
+                throw new NpgsqlException(ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ Cuestionarios/Cuestionarios/DAL/IRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace Cuestionarios.DAL
+{
+    interface IRepository<TEntity> where TEntity: class
+    {
+        void Add(TEntity pEntity);
+        void Remove(TEntity pEntity);
+        TEntity GetByID(int pId);
+        IEnumerable<TEntity> GetAll();
+    }
+}
