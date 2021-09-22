1a807b76fce4ddcc2e93acc71e1c386584aba5bd
nahuelmontesino <montesinonahuel@gmail.com>
2021-09-22 16:42:54 -0300

    interface with base properties and methods for different sources

---
+++ Cuestionarios/Cuestionarios/Source/ISource.cs
@@ -0,0 +1,14 @@
+﻿using Cuestionarios.Entities;
+using System.Collections.Generic;
+
+namespace Cuestionarios.Source
+{
+    public interface ISource
+    {
+        string Url { get; }
+        string Name { get; }
+        Dictionary<int, string> CategoryDictionary { get; }
+        Dictionary<int, string> DifficultyDictionary { get; }
+        List<Question> GetQuestions(string pDificulty, int pCategory, int pAmount);
+    }
+}
