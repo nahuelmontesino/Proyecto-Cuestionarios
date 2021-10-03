241a70264b5702d265ec8812e429fd9a4b031c31
ARaffo <49041262+ARaffo@users.noreply.github.com>
2021-10-03 16:12:03 -0300

    Feat: form login create

--- Cuestionarios/UI/Form1.cs
+++ Cuestionarios/UI/Form1.cs
@@ -15,6 +15,27 @@ namespace UI
         public Form1()
         {
             InitializeComponent();
+         
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+            g.DrawLine(pen, 20, 10, 300, 100);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login login = new Login();
+            login.ShowDialog();
+            this.Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
---
+++ Cuestionarios/UI/Login.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void txtuser_Enter(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "User")
+            {
+                txtuser.Text = "";
+                txtuser.ForeColor = Color.LightGray;
+
+
+            }
+        }
+
+        private void txtuser_Leave(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "")
+            {
+                txtuser.Text = "User";
+                txtuser.ForeColor = Color.DimGray;
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void texpass_Enter(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "Password")
+            {
+                txtpass.Text = "";
+                txtpass.ForeColor = Color.LightGray;
+                txtpass.UseSystemPasswordChar = true;
+
+            }
+        }
+
+        private void texpass_Leave(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "")
+            {
+                txtpass.Text = "Password";
+                txtpass.ForeColor = Color.DimGray;
+                txtpass.UseSystemPasswordChar = false;
+
+            }
+        }
+    }
+}
