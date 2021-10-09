bddedf10ab196b752f9bdbffd106e6b5e550dcf2
nahuelmontesino <montesinonahuel@gmail.com>
2021-10-09 15:50:24 -0300

    unnecessary method

--- Cuestionarios/Cuestionarios/DAL/SetRepository.cs
+++ Cuestionarios/Cuestionarios/DAL/SetRepository.cs
@@ -25,22 +25,5 @@ namespace Cuestionarios.DAL
                 throw new NpgsqlException("Error trying to get set name: ", ex);
             }
         }
-
-        /// <summary>
-        /// Adds a question to the database
-        /// </summary>
-        public void AddQuestion(Question pQuestion)
-        {
-            try
-            {
-                iDbContext.Questions.Attach(pQuestion);
-                iDbContext.Questions.Add(pQuestion);
-                iDbContext.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                throw new NpgsqlException("Error trying to add questions: ", ex);
-            }
-        }
     }
 }
