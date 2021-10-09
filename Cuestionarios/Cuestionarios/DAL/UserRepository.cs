b991f55a2354ad0a90a39159dc5337954a7c2935
nahuelmontesino <montesinonahuel@gmail.com>
2021-10-09 16:18:36 -0300

    feat: repository for users

---
+++ Cuestionarios/Cuestionarios/DAL/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using Cuestionarios.Domain;
+using Npgsql;
+using System;
+using System.Linq;
+
+
+namespace Cuestionarios.DAL
+{
+    class UserRepository: Repository<User, QuestionnaireDbContext>
+    {
+        public UserRepository(QuestionnaireDbContext pContext) : base(pContext)
+        {
+        }
+        /// <summary>
+        /// Gets User by username
+        /// </summary>
+        public User GetByUserName(string pUserName)
+        {
+            try
+            {
+                return Get(user => user.Username == pUserName).Single();
+            }
+            catch (Exception ex)
+            {
+                throw new NpgsqlException(ex.ToString());
+            }
+        }
+    }
+}
